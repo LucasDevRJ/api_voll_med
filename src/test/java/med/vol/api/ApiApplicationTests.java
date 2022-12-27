e0d8ce1fc15ef1f5f520d91f11d015d6256897d8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-27 11:46:56 -0300

    Início do projeto.

---
+++ src/test/java/med/vol/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package med.vol.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
