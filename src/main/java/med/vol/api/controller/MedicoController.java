b6091cc59a5eb2f6b24ed1f02320ea33dc31736d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-27 16:05:40 -0300

    Adição da classe DadosCadastroMedico para pegar os atributos do medico e do enumerador Especialidade para serem as especilidades do medico.

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.vol.api.controller;
 
+import med.vol.api.medico.DadosCadastroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
     @PostMapping
-    public void cadastrar(@RequestBody String cep) {
-        System.out.println(cep);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        System.out.println(dados);
     }
 }
---
+++ src/main/java/med/vol/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,4 @@
+package med.vol.api.medico;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade) {
+}
