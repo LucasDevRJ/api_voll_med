1f37f30f1cf06e4b27fa50bd5a13ae0dcc4e5680
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-27 15:53:34 -0300

    Pegando dados de um arquivo JSON.

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -1,4 +1,15 @@
 package med.vol.api.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("medicos")
 public class MedicoController {
+    @PostMapping
+    public void cadastrar(@RequestBody String cep) {
+        System.out.println(cep);
+    }
 }
