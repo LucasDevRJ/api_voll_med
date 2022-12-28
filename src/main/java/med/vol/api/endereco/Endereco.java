23f8bac14d310761b22f7143fb1dc74991624b49
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-28 17:08:02 -0300

    Criação da classe Endereco com seus atributos.

--- src/main/java/med/vol/api/endereco/Endereco.java
+++ src/main/java/med/vol/api/endereco/Endereco.java
@@ -1,4 +1,12 @@
 package med.vol.api.endereco;
 
 public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
 }
