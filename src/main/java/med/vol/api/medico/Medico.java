824e9a269966ff9cbcc6ea1abf10df616bb65411
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-28 16:59:47 -0300

    Criação dos atributos da classe Medico e da classe Endereco.

--- src/main/java/med/vol/api/medico/Medico.java
+++ src/main/java/med/vol/api/medico/Medico.java
@@ -1,4 +1,13 @@
 package med.vol.api.medico;
 
+import med.vol.api.endereco.Endereco;
+
 public class Medico {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    private Especialidade especialidade;
+    private Endereco endereco;
 }
