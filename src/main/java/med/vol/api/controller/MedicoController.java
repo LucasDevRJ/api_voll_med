96ab7e285c0ccebd00bf5328013c664f969e5e02
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-02 17:53:11 -0300

    Criação do Record Dados do médico para listar conteúdo.

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -3,13 +3,13 @@ package med.vol.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.vol.api.medico.DadosCadastroMedico;
+import med.vol.api.medico.DadosListagemMedico;
 import med.vol.api.medico.Medico;
 import med.vol.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -21,5 +21,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll();
     }
 }
