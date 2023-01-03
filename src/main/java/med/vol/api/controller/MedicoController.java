4ee9b9bfe6e82b2c10f54b3f9386412cf4dfb687
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-03 18:03:14 -0300

    Adição do método atualizar.

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -28,4 +28,10 @@ public class MedicoController {
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosCadastroMedico dados) {
+
+    }
 }
