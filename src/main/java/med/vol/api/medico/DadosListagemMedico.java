d9942d3155005288a4222dc9bafe60475f09a05a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-03 17:56:43 -0300

    Listagem com ID.

--- src/main/java/med/vol/api/medico/DadosListagemMedico.java
+++ src/main/java/med/vol/api/medico/DadosListagemMedico.java
@@ -1,8 +1,8 @@
 package med.vol.api.medico;
 
-public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
 
     public DadosListagemMedico(Medico medico) {
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 }
