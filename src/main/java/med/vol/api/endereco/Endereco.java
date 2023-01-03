0e1dda251888b40bd97bc31ee9dc0895de748df9
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-03 18:48:37 -0300

    Adição do método para atualizar informações do endereço.

--- src/main/java/med/vol/api/endereco/Endereco.java
+++ src/main/java/med/vol/api/endereco/Endereco.java
@@ -28,4 +28,34 @@ public class Endereco {
         this.cidade = dados.cidade();
         this.uf = dados.uf();
     }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+        if (dados.logradouro() != null) {
+            this.logradouro = dados.logradouro();
+        }
+
+        if (dados.bairro() != null) {
+            this.bairro = dados.bairro();
+        }
+
+        if (dados.cep() != null) {
+            this.cep = dados.cep();
+        }
+
+        if (dados.numero() != null) {
+            this.numero = dados.numero();
+        }
+
+        if (dados.complemento() != null) {
+            this.complemento = dados.complemento();
+        }
+
+        if (dados.cidade() != null) {
+            this.cidade = dados.cidade();
+        }
+
+        if (dados.uf() != null) {
+            this.uf = dados.uf();
+        }
+    }
 }
--- src/main/java/med/vol/api/medico/Medico.java
+++ src/main/java/med/vol/api/medico/Medico.java
@@ -39,6 +39,16 @@ public class Medico {
     }
 
     public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
 
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
     }
 }
