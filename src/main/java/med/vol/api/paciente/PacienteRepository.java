b25f0993108b1c79403067d85b516db23b9adbf7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-04 19:21:00 -0300

    Conexão com a entidade Paciente.

--- src/main/java/med/vol/api/paciente/PacienteRepository.java
+++ src/main/java/med/vol/api/paciente/PacienteRepository.java
@@ -1,4 +1,6 @@
 package med.vol.api.paciente;
 
-public interface PacienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
