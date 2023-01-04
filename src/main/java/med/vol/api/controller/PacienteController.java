e1cc4874045791fa446aff89893b10e780187a81
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-04 16:27:29 -0300

    Criação do método para cadastrar pacientes.

--- src/main/java/med/vol/api/controller/PacienteController.java
+++ src/main/java/med/vol/api/controller/PacienteController.java
@@ -1,5 +1,15 @@
 package med.vol.api.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pacientes")
 public class PacienteController {
 
+    @PostMapping
+    public void cadastrar() {
+
+    }
 }
