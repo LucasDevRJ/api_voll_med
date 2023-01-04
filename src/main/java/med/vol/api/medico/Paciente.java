7e92cc3b28288d23dd3c122d6ded24542254bfaa
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-04 12:02:42 -0300

    Adição dos atributos da classe Paciente.

--- src/main/java/med/vol/api/medico/Paciente.java
+++ src/main/java/med/vol/api/medico/Paciente.java
@@ -1,4 +1,13 @@
 package med.vol.api.medico;
 
+import med.vol.api.endereco.Endereco;
+
 public class Paciente {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private Endereco endereco;
 }
