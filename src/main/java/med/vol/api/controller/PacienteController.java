ee76da949e3c9674537d9e8f87781203acf98491
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-05 12:07:54 -0300

    Utilizando o Bean Validation na classe de DadosCadastroPaciente.java

--- src/main/java/med/vol/api/controller/PacienteController.java
+++ src/main/java/med/vol/api/controller/PacienteController.java
@@ -4,6 +4,7 @@ import med.vol.api.paciente.DadosCadastroPaciente;
 import med.vol.api.paciente.Paciente;
 import med.vol.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class PacienteController {
     private PacienteRepository repository;
 
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
     }
--- src/main/java/med/vol/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/vol/api/paciente/DadosCadastroPaciente.java
@@ -1,7 +1,27 @@
 package med.vol.api.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.vol.api.endereco.DadosEndereco;
 import med.vol.api.endereco.Endereco;
 
-public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf, DadosEndereco endereco) {
+public record DadosCadastroPaciente(
+
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{11}")
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{11}")
+        String cpf,
+        @NotNull
+        @Valid
+        DadosEndereco endereco) {
 }
