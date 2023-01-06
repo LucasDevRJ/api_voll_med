be0d3cd7b04f51958371c0e39f01b2b19a4cfdec
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-06 10:36:11 -0300

    Método de listar pacientes concluído.

--- src/main/java/med/vol/api/controller/PacienteController.java
+++ src/main/java/med/vol/api/controller/PacienteController.java
@@ -7,10 +7,9 @@ import med.vol.api.paciente.Paciente;
 import med.vol.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("pacientes")
@@ -25,7 +24,8 @@ public class PacienteController {
         repository.save(new Paciente(dados));
     }
 
+    @GetMapping
     public List<DadosListagemPaciente> listar() {
-        return repository.findAll().stream().map(DadosListagemPaciente::new);
+        return repository.findAll().stream().map(DadosListagemPaciente::new).toList();
     }
 }
