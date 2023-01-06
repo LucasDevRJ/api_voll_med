e7725e1bd7e9754aad6ec94dd11f98eb37ffca01
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-06 20:32:11 -0300

    Ordenação da listagem por nomes.

--- src/main/java/med/vol/api/controller/PacienteController.java
+++ src/main/java/med/vol/api/controller/PacienteController.java
@@ -6,6 +6,9 @@ import med.vol.api.paciente.DadosListagemPaciente;
 import med.vol.api.paciente.Paciente;
 import med.vol.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +28,7 @@ public class PacienteController {
     }
 
     @GetMapping
-    public List<DadosListagemPaciente> listar() {
-        return repository.findAll().stream().map(DadosListagemPaciente::new).toList();
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
