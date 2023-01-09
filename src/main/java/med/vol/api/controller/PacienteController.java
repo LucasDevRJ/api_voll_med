0442c4054b670d59ecea1c582f89ceeb0b77235c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-09 11:20:43 -0300

    Método de exclusão de pacientes pela ID criado.

--- src/main/java/med/vol/api/controller/PacienteController.java
+++ src/main/java/med/vol/api/controller/PacienteController.java
@@ -35,4 +35,10 @@ public class PacienteController {
         var paciente = repository.getReferenceById(dados.id());
         paciente.atualizarInformacoes(dados);
     }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
