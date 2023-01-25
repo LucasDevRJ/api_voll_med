20563ec70b83e3863c5f8168610f38efa1606e08
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-25 19:39:54 -0300

    Passagem de atributos no record de DadosDetalhamentoPaciente

--- src/main/java/med/vol/api/paciente/DadosDetalhamentoPaciente.java
+++ src/main/java/med/vol/api/paciente/DadosDetalhamentoPaciente.java
@@ -1,4 +1,8 @@
 package med.vol.api.paciente;
 
-public record DadosDetalhamentoPaciente(Paciente paciente) {
+import med.vol.api.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(Long id, String nome, String email, String telefone, String cpf, Endereco endereco) {
+
+
 }
