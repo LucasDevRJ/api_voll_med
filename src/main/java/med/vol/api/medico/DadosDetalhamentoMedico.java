59f16200684c9135403ec61286062842c4ef9ad8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-25 17:24:18 -0300

    DTO dos dados dos médicos para listagem concluído com sucesso.

--- src/main/java/med/vol/api/medico/DadosDetalhamentoMedico.java
+++ src/main/java/med/vol/api/medico/DadosDetalhamentoMedico.java
@@ -1,4 +1,10 @@
 package med.vol.api.medico;
 
-public record DadosDetalhamentoMedico(Medico medico) {
+import med.vol.api.endereco.Endereco;
+
+public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
+
+    public DadosDetalhamentoMedico(Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+    }
 }
