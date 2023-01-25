1a4a0a025b37084e32d568b1658c1f142233cca7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-25 19:37:15 -0300

    Alterações no método de cadastrar novo paciente, colocando URI e o corpo de dados e também o DTO.

--- src/main/java/med/vol/api/controller/PacienteController.java
+++ src/main/java/med/vol/api/controller/PacienteController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -20,8 +22,13 @@ public class PacienteController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
-        repository.save(new Paciente(dados));
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados, UriComponentsBuilder uriBuilder) {
+        var paciente = new Paciente(dados);
+        repository.save(paciente);
+
+        var uri = uriBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
     }
 
     @GetMapping
---
+++ src/main/java/med/vol/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,4 @@
+package med.vol.api.paciente;
+
+public record DadosDetalhamentoPaciente(Paciente paciente) {
+}
