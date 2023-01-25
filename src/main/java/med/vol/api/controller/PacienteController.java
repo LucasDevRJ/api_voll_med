d951a9cb1b1a8b89c94f4472e17f389df285ff73
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-25 20:10:53 -0300

    Método para detalhar dados de um paciente criado com sucesso.

--- src/main/java/med/vol/api/controller/PacienteController.java
+++ src/main/java/med/vol/api/controller/PacienteController.java
@@ -56,4 +56,11 @@ public class PacienteController {
 
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var paciente = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
+    }
 }
