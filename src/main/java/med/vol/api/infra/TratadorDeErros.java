202ac1ec811a618575b97e87599e90035eb9f9f5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-26 18:55:57 -0300

    Adição do método que tratar o erro 404 dos métodos.

--- src/main/java/med/vol/api/infra/TratadorDeErros.java
+++ src/main/java/med/vol/api/infra/TratadorDeErros.java
@@ -1,4 +1,15 @@
 package med.vol.api.infra;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
 }
