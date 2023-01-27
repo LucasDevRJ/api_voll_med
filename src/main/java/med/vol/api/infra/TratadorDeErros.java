f0af62ff185d4246d26b483bbf7d60e41b21c7a5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-27 12:42:48 -0300

    Criação do método para tratar o erro 400.

--- src/main/java/med/vol/api/infra/TratadorDeErros.java
+++ src/main/java/med/vol/api/infra/TratadorDeErros.java
@@ -2,6 +2,7 @@ package med.vol.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +12,10 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400() {
+        return ResponseEntity.badRequest().build();
     }
 }
