26fdc8499b70412baa480bd1175b34de1f1bc5c0
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-27 12:54:18 -0300

    Mensagem de erro da requisição formatada.

--- src/main/java/med/vol/api/infra/TratadorDeErros.java
+++ src/main/java/med/vol/api/infra/TratadorDeErros.java
@@ -2,6 +2,7 @@ package med.vol.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,6 +18,12 @@ public class TratadorDeErros {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
         var erros = ex.getFieldErrors();
-        return ResponseEntity.badRequest().body(erros);
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
