a630d280782a0780681cbebfc5f4cd57088badcf
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-27 19:54:52 -0300

    Atribuindo cada mensagem de erro ao seu devido campo.

--- src/main/java/med/vol/api/domain/medico/DadosCadastroMedico.java
+++ src/main/java/med/vol/api/domain/medico/DadosCadastroMedico.java
@@ -8,20 +8,22 @@ import jakarta.validation.constraints.Pattern;
 import med.vol.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
-        @NotBlank
+        @NotBlank(message = "{nome.obrigatorio}")
         String nome,
-        @NotBlank
-        @Email
+
+        @NotBlank(message = "{email.obrigatorio}")
+        @Email(message = "{email.invalido}")
         String email,
 
-        @NotBlank
+        @NotBlank(message = "{telefone.obrigatorio}")
         String telefone,
-        @NotBlank
-        @Pattern(regexp = "\\d{4,6}")
+
+        @NotBlank(message = "{crm.obrigatorio}")
+        @Pattern(regexp = "\\d{4,6}", message = "{crm.invalido}")
         String crm,
-        @NotNull
+
+        @NotNull(message = "{especialidade.obrigatoria}")
         Especialidade especialidade,
-        @NotNull
-        @Valid
-        DadosEndereco endereco) {
-}
+
+        @NotNull(message = "{endereco.obrigatorio}")
+        @Valid DadosEndereco endereco) {}
