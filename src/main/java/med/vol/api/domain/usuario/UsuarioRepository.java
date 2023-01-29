94321887b2de6368f1476c0b66768c7fcfa05591
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-29 15:46:06 -0300

    Classe para configurar o Security.

--- src/main/java/med/vol/api/domain/usuario/UsuarioRepository.java
+++ src/main/java/med/vol/api/domain/usuario/UsuarioRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    UserDetails findByLogin(String username);
+    UserDetails findByLogin(String login);
 }
--- src/main/java/med/vol/api/infra/exception/TratadorDeErros.java
+++ src/main/java/med/vol/api/infra/exception/TratadorDeErros.java
@@ -1,4 +1,4 @@
-package med.vol.api.infra;
+package med.vol.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
