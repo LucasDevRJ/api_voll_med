33b4657802fb26ffb3cbb7c3dfb5946aa588c638
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-29 13:51:13 -0300

    Adição das anotações da entidade.

--- src/main/java/med/vol/api/domain/usuario/Usuario.java
+++ src/main/java/med/vol/api/domain/usuario/Usuario.java
@@ -1,5 +1,18 @@
 package med.vol.api.domain.usuario;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Usuario {
 
     private Long id;
