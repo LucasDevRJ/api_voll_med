899dd61150b9cea54ef763cfa93c407707db25d1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-29 15:26:05 -0300

    Criação do método para procurar login do usuário.

--- src/main/java/med/vol/api/domain/usuario/UsuarioRepository.java
+++ src/main/java/med/vol/api/domain/usuario/UsuarioRepository.java
@@ -1,6 +1,8 @@
 package med.vol.api.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String username);
 }
