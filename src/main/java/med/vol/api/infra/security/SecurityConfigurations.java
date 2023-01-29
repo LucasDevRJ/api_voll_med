65bddfe3fb7fc32f29762bbef4f8beeafee9d90b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-29 16:06:20 -0300

    Colocando o login como Stateless.

--- src/main/java/med/vol/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/vol/api/infra/security/SecurityConfigurations.java
@@ -1,4 +1,20 @@
 package med.vol.api.infra.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().build();
+    }
 }
