859ad885a7ccafc88a480b35f1779d2cfecadf7d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-31 14:53:27 -0300

    Criptografia de senha nos usuários.

--- src/main/java/med/vol/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/vol/api/infra/security/SecurityConfigurations.java
@@ -1,10 +1,15 @@
 package med.vol.api.infra.security;
 
+import med.vol.api.controller.AutenticacaoController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,5 +21,14 @@ public class SecurityConfigurations {
         return http.csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().build();
+    }
+
+    @Bean
+    public AuthenticationManager autenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
