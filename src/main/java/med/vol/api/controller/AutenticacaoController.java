ba9f4b61eb3d66bcf4667d9f199e14ff74af2aaa
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-31 14:14:29 -0300

    Adição do record de autenticação dos dados dos usuários.

--- src/main/java/med/vol/api/controller/AutenticacaoController.java
+++ src/main/java/med/vol/api/controller/AutenticacaoController.java
@@ -1,4 +1,19 @@
 package med.vol.api.controller;
 
+import jakarta.validation.Valid;
+import med.vol.api.domain.usuario.DadosAutenticacao;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class AutenticacaoController {
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+
+    }
 }
---
+++ src/main/java/med/vol/api/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,4 @@
+package med.vol.api.domain.usuario;
+
+public record DadosAutenticacao() {
+}
