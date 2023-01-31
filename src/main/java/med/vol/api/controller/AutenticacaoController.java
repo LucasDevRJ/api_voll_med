9eb78475d702d27abe3a25fcedc35ba4b85cabca
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-31 14:19:37 -0300

    Injeção de dependência.

--- src/main/java/med/vol/api/controller/AutenticacaoController.java
+++ src/main/java/med/vol/api/controller/AutenticacaoController.java
@@ -2,7 +2,9 @@ package med.vol.api.controller;
 
 import jakarta.validation.Valid;
 import med.vol.api.domain.usuario.DadosAutenticacao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/login")
 public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
--- src/main/java/med/vol/api/domain/usuario/DadosAutenticacao.java
+++ src/main/java/med/vol/api/domain/usuario/DadosAutenticacao.java
@@ -1,4 +1,4 @@
 package med.vol.api.domain.usuario;
 
-public record DadosAutenticacao() {
+public record DadosAutenticacao(String login, String senha) {
 }
