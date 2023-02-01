faeef528815cf6a9215c9ad084eb57cf9ae7ae7e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-02-01 11:29:29 -0300

    Criação do método para gerar token e data de expiração.

--- src/main/java/med/vol/api/infra/security/TokenService.java
+++ src/main/java/med/vol/api/infra/security/TokenService.java
@@ -1,4 +1,33 @@
 package med.vol.api.infra.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.vol.api.domain.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
 public class TokenService {
+
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algoritmo = Algorithm.HMAC256("12345678");
+            String token = JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("erro ao gerar token jwt", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
 }
