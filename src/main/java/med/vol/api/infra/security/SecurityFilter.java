1d13b68ed57c7200a61ac47e30394fd40383b079
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-02-03 18:38:05 -0300

    Filtro chamando outro filtro.

--- src/main/java/med/vol/api/infra/security/SecurityFilter.java
+++ src/main/java/med/vol/api/infra/security/SecurityFilter.java
@@ -17,6 +17,6 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        System.out.println("Filtro chamado");
+        filterChain.doFilter(request, response);
     }
 }
