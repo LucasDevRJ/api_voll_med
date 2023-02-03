13d06501f03a8efdfc7537f48c9adb20f2c22e7f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-02-03 19:07:33 -0300

    Métodos só podem ser acessados com o token.

--- src/main/java/med/vol/api/infra/security/SecurityFilter.java
+++ src/main/java/med/vol/api/infra/security/SecurityFilter.java
@@ -18,9 +18,17 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
 
+        System.out.println(tokenJWT);
+
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        }
+
+        return authorizationHeader.replace("Bearer", "");
     }
 }
