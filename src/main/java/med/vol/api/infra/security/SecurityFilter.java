1488bd6358445c4325914d8fb5ed7ebde33bdbcd
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-02-07 10:17:57 -0300

    Adição do método verificar token.

--- src/main/java/med/vol/api/infra/security/SecurityFilter.java
+++ src/main/java/med/vol/api/infra/security/SecurityFilter.java
@@ -18,8 +18,6 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
 
-        System.out.println(tokenJWT);
-
         filterChain.doFilter(request, response);
     }
 
--- src/main/java/med/vol/api/infra/security/TokenService.java
+++ src/main/java/med/vol/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.vol.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.vol.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,19 @@ public class TokenService {
                     .sign(algoritmo);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
