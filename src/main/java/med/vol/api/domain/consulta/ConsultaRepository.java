379cf9b890eda64526b7cf70ca5de9062e6e6e6d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-21 15:27:00 -0300

    Criação da interface repositória da consulta.

---
+++ src/main/java/med/vol/api/domain/consulta/ConsultaRepository.java
@@ -0,0 +1,6 @@
+package med.vol.api.domain.consulta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+}
