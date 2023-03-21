75ad5896b01eb2b7b41c1968b6b3e37217fdccc4
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-21 15:24:30 -0300

    Adição da classe consulta.

---
+++ src/main/java/med/vol/api/domain/consulta/Consulta.java
@@ -0,0 +1,32 @@
+package med.vol.api.domain.consulta;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.vol.api.domain.medico.Medico;
+import med.vol.api.domain.paciente.Paciente;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode (of = "id")
+public class Consulta {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime data;
+}
