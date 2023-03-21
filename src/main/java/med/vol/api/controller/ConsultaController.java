167df3fd888ae43c2c5f8788830e6424cbac9773
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-21 15:02:40 -0300

    Criação da classe para agendar consultas.

---
+++ src/main/java/med/vol/api/controller/ConsultaController.java
@@ -0,0 +1,17 @@
+package med.vol.api.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("consultas")
+public class ConsultaController {
+
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        System.out.println(dados);
+        return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
+    }
+}
