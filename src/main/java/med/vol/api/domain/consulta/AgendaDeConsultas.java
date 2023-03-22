ccc579afa3204e158edf8c6daef91647f39d8b18
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-22 19:15:07 -0300

    Injeções de dependências na classe de agendar consultas.

--- src/main/java/med/vol/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/vol/api/domain/consulta/AgendaDeConsultas.java
@@ -1,11 +1,27 @@
 package med.vol.api.domain.consulta;
 
+import med.vol.api.domain.medico.MedicoRepository;
+import med.vol.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgendaDeConsultas {
 
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
     public void agendar(DadosAgendamentoConsulta dados) {
 
+
+
+        var consulta = new Consulta(null, );
+        consultaRepository.save(consulta);
     }
 }
