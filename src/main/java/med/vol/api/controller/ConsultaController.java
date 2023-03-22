272a5be920fd1a6e1c9cefaf8e24d15473b29bef
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-22 19:04:48 -0300

    Injeção de dependência do método para agendar.

--- src/main/java/med/vol/api/controller/ConsultaController.java
+++ src/main/java/med/vol/api/controller/ConsultaController.java
@@ -1,8 +1,10 @@
 package med.vol.api.controller;
 
 import jakarta.validation.Valid;
+import med.vol.api.domain.consulta.AgendaDeConsultas;
 import med.vol.api.domain.consulta.DadosAgendamentoConsulta;
 import med.vol.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("consultas")
 public class ConsultaController {
 
+    @Autowired
+    private AgendaDeConsultas agenda;
+
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        agenda.agendar(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
     }
 }
--- src/main/java/med/vol/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/vol/api/domain/consulta/AgendaDeConsultas.java
@@ -1,4 +1,11 @@
 package med.vol.api.domain.consulta;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AgendaDeConsultas {
+
+    public void agendar(DadosAgendamentoConsulta dados) {
+
+    }
 }
