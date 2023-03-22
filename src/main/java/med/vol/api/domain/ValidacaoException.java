3bc5ad1e3b2a4d966133f0c65550c9b80e881566
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-22 19:27:42 -0300

    Criação do erro com mensagem personalizada.

---
+++ src/main/java/med/vol/api/domain/ValidacaoException.java
@@ -0,0 +1,6 @@
+package med.vol.api.domain;
+
+public class ValidacaoException extends Throwable {
+    public ValidacaoException(String s) {
+    }
+}
--- src/main/java/med/vol/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/vol/api/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,6 @@
 package med.vol.api.domain.consulta;
 
+import med.vol.api.domain.ValidacaoException;
 import med.vol.api.domain.medico.MedicoRepository;
 import med.vol.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,10 @@ public class AgendaDeConsultas {
     private PacienteRepository pacienteRepository;
 
     public void agendar(DadosAgendamentoConsulta dados) {
+
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Id do paciente informado não existe!");
+        }
 
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
         var medico = medicoRepository.findById(dados.idMedico()).get();
