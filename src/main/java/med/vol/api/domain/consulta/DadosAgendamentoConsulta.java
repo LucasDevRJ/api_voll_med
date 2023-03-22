7193ce05a768654cb959c6fdc9f80b2de0783792
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-22 19:58:30 -0300

    Adição da Query para pegar todos os médicos de tal especialidade.

--- src/main/java/med/vol/api/domain/consulta/DadosAgendamentoConsulta.java
+++ src/main/java/med/vol/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -2,6 +2,7 @@ package med.vol.api.domain.consulta;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.vol.api.domain.medico.Especialidade;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +14,8 @@ public record DadosAgendamentoConsulta(
 
         @NotNull
         @Future
-        LocalDateTime data
+        LocalDateTime data,
+
+        Especialidade especialidade
 ) {
 }
--- src/main/java/med/vol/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/vol/api/domain/medico/MedicoRepository.java
@@ -3,8 +3,20 @@ package med.vol.api.domain.medico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+
+    @Query("""
+            select m from Medico m
+            where
+            m.ativo = 1
+            and
+            m.especialidade = :especialidade
+            """)
+    Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
 }
