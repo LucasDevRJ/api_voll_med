7095ade3ecc30e44a25ddcfc2df16ca29970b6e5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-22 20:07:17 -0300

    Adição de Query para a especialidade do médico.

--- src/main/java/med/vol/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/vol/api/domain/medico/MedicoRepository.java
@@ -17,6 +17,14 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             m.ativo = 1
             and
             m.especialidade = :especialidade
+            and
+            m.id not in(
+                select c.medico.id from Consulta c
+                where
+                c.data = :data
+            )
+            order by rand()
+            limit 1
             """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
 }
