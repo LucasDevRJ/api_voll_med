43a4d8c43623ebb045c15c3427bf0a7331444d6f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-23 17:43:43 -0300

    Criação do método para validar se o médico está ativo ou não.

--- src/main/java/med/vol/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
+++ src/main/java/med/vol/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
@@ -1,4 +1,23 @@
 package med.vol.api.domain.consulta.validacoes;
 
+import med.vol.api.domain.ValidacaoException;
+import med.vol.api.domain.consulta.DadosAgendamentoConsulta;
+import med.vol.api.domain.medico.MedicoRepository;
+
 public class ValidadorMedicoAtivo {
+
+    private MedicoRepository medicoRepository;
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        //escolha do médico opcional
+        if (dados.idMedico() == null) {
+            return;
+        }
+
+        var medicoEstaAtivo = medicoRepository.findAtivoById(dados.idMedico());
+
+        if (!medicoEstaAtivo) {
+            throw new ValidacaoException("Consulta não pode ser agendada com médico excluído");
+        }
+    }
 }
