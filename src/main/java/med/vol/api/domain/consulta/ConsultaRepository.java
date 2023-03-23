135ac5d9ff615e12adc72b43cd0a4a353a671bfc
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-23 18:42:36 -0300

    Validação para saber se tem mais de um horário marcado no mesmo horário.

--- src/main/java/med/vol/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/vol/api/domain/consulta/ConsultaRepository.java
@@ -6,4 +6,6 @@ import java.time.LocalDateTime;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
     boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
+
+    boolean existsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
 }
---
+++ src/main/java/med/vol/api/domain/consulta/validacoes/ValidadorPacienteSemOutraConsultaNoDia.java
@@ -0,0 +1,19 @@
+package med.vol.api.domain.consulta.validacoes;
+
+import med.vol.api.domain.ValidacaoException;
+import med.vol.api.domain.consulta.ConsultaRepository;
+import med.vol.api.domain.consulta.DadosAgendamentoConsulta;
+
+public class ValidadorPacienteSemOutraConsultaNoDia {
+
+    private ConsultaRepository consultaRepository;
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var primeiroHorario = dados.data().withHour(7);
+        var ultimoHorario = dados.data().withHour(18);
+        var pacientePossuiOutraConsultaNoDia = consultaRepository.existsByPacienteIdAndDataBetween(dados.idPaciente(), primeiroHorario, ultimoHorario);
+        if (pacientePossuiOutraConsultaNoDia) {
+            throw new ValidacaoException("Paciente já possui uma consulta agendada nesse dia");
+        }
+    }
+}
