63d82e47def9a8b0d2e14502f5218dd707a650b6
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-23 17:34:58 -0300

    Validação para horário com antecedência.

---
+++ src/main/java/med/vol/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,20 @@
+package med.vol.api.domain.consulta.validacoes;
+
+import med.vol.api.domain.ValidacaoException;
+import med.vol.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioAntecedencia {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.data();
+        var agora = LocalDateTime.now();
+        var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
+
+        if (diferencaEmMinutos < 30) {
+            throw new ValidacaoException("Consulta deve ser agendada com antecedência mínima de 30 minutos");
+        }
+    }
+}
