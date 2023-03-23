00568649ee294151cdfb7294cc4353e0a73fa18a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-23 17:27:15 -0300

    Adição do método para validar horário.

--- src/main/java/med/vol/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
+++ src/main/java/med/vol/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -1,4 +1,21 @@
 package med.vol.api.domain.consulta.validacoes;
 
+import med.vol.api.domain.ValidacaoException;
+import med.vol.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.DayOfWeek;
+
 public class ValidadorHorarioFuncionamentoClinica {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.data();
+
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesDaAberturaDaClinica = dataConsulta.getHour() < 7;
+        var depoisDoEncerramentoDaClinica = dataConsulta.getHour() > 18;
+
+        if (domingo || antesDaAberturaDaClinica || depoisDoEncerramentoDaClinica) {
+            throw new ValidacaoException("Consulta fora do horário de funcionamento da clínica!");
+        }
+    }
 }
