b1caf111fd771a032440f292c64d0ff94aacdc7b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-23 17:51:39 -0300

    Criação do método e classe para verificar se já existe um médico com horário marcado.

--- src/main/java/med/vol/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/vol/api/domain/consulta/ConsultaRepository.java
@@ -2,5 +2,8 @@ package med.vol.api.domain.consulta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
 }
---
+++ src/main/java/med/vol/api/domain/consulta/validacoes/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
@@ -0,0 +1,18 @@
+package med.vol.api.domain.consulta.validacoes;
+
+import med.vol.api.domain.ValidacaoException;
+import med.vol.api.domain.consulta.ConsultaRepository;
+import med.vol.api.domain.consulta.DadosAgendamentoConsulta;
+
+public class ValidadorMedicoComOutraConsultaNoMesmoHorario {
+
+    private ConsultaRepository consultaRepository;
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var medicoPossuiOutraConsultaNoMesmoHorario = consultaRepository.existsByMedicoIdAndData(dados.idMedico(), dados.data());
+
+        if (medicoPossuiOutraConsultaNoMesmoHorario) {
+            throw new ValidacaoException("Médico já possui outra consulta agendada nesse mesmo horário");
+        }
+    }
+}
