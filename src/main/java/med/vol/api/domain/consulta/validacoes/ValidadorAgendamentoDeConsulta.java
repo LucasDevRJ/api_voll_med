4d5a9c29837d2c7bfc5833d4234072f03a5e64e5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-24 20:45:40 -0300

    Adição do contrato de implementação para validar.

--- src/main/java/med/vol/api/domain/consulta/validacoes/ValidadorAgendamentoDeConsulta.java
+++ src/main/java/med/vol/api/domain/consulta/validacoes/ValidadorAgendamentoDeConsulta.java
@@ -1,4 +1,8 @@
 package med.vol.api.domain.consulta.validacoes;
 
+import med.vol.api.domain.consulta.DadosAgendamentoConsulta;
+
 public interface ValidadorAgendamentoDeConsulta {
+
+    void validar(DadosAgendamentoConsulta dados);
 }
