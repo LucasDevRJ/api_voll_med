a2fb1b54127bbc6f15f16715cee8297fa7057f5e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-24 21:46:46 -0300

    Conserto de lógica nas Query

--- src/main/java/med/vol/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/vol/api/domain/paciente/PacienteRepository.java
@@ -3,9 +3,16 @@ package med.vol.api.domain.paciente;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
 
-    boolean findAtivoById(Long idPaciente);
+    @Query("""
+            select p.ativo
+            from Paciente p
+            where
+            p.id = :id
+            """)
+    boolean findAtivoById(Long id);
 }
