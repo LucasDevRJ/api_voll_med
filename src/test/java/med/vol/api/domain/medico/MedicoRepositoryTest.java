01c0ef1bb360945357ee77269cc2eac978042d45
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-28 17:38:02 -0300

    Teste JUnit no medico respository.

---
+++ src/test/java/med/vol/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,12 @@
+package med.vol.api.domain.medico;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedicoRepositoryTest {
+
+    @Test
+    void escolherMedicoAleatorioLivreNaData() {
+    }
+}
