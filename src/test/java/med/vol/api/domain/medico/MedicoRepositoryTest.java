2b48e4d8f1efc7dd31c7a8c314d4fa98d0381857
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-28 18:11:07 -0300

    Arquivo application para os testes unitários.

--- src/test/java/med/vol/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/vol/api/domain/medico/MedicoRepositoryTest.java
@@ -1,9 +1,13 @@
 package med.vol.api.domain.medico;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MedicoRepositoryTest {
 
     @Test
