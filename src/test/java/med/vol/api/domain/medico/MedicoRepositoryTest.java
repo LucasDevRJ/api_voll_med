c5144c9b7c8332023d320ed2d97444da5dfe8238
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-28 18:26:04 -0300

    Testes automatizados configurados com sucesso.

--- src/test/java/med/vol/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/vol/api/domain/medico/MedicoRepositoryTest.java
@@ -3,11 +3,13 @@ package med.vol.api.domain.medico;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class MedicoRepositoryTest {
 
     @Test
